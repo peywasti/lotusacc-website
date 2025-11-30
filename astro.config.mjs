d7ee1684c536e6275b2b941d6ea926701ff4336a
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-11-30 22:20:38 +0330

    config for server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: vercel(),
 });
