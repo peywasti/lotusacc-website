d7b0e082a4f9deb933402b18f67b915c7b70e350
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-11-30 20:29:56 +0330

    add tailwind config for font

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Estedad", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
