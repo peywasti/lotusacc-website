9e96738a9879f1528dec858d7e8f7059f8bdb017
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-02 21:17:29 +0330

    add primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          700: '#18486e',
+          800: '#0f3a5a',
+        },
+      },
       fontFamily: {
         sans: ["Estedad", "sans-serif"],
       },
